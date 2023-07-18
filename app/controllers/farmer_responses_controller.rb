4618da15c5ded52344f4f317886f3ba0fb4a6b1b
JohnOkari <johnnokari@gmail.com>
2023-07-18 22:43:42 +0300

    add farmer_response controller

--- app/controllers/farmer_responses_controller.rb
+++ app/controllers/farmer_responses_controller.rb
@@ -1,2 +1,21 @@
 class FarmerResponsesController < ApplicationController
-end
+    def new
+      @farmer_response = FarmerResponse.new
+    end
+  
+    def create
+      @farmer_response = FarmerResponse.new(farmer_response_params)
+      if @farmer_response.save
+        # Add logic to facilitate communication with buyers and update order details here
+        redirect_to root_path, notice: "Farmer response sent successfully."
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def farmer_response_params
+      params.require(:farmer_response).permit(:message).merge(farmer_id: current_farmer.id)
+    end
+  end
