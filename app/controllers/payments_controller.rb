c2dd766a14a24f11f363eabef4f98aa8e6ccfee5
JohnOkari <johnnokari@gmail.com>
2023-07-24 19:46:56 +0300

    add payments logic

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,7 +7,22 @@ class PaymentsController < ApplicationController
       @payment = Payment.new(payment_params)
       if @payment.save
         # Add logic to handle payment processing here
-        redirect_to payment_path(@payment), notice: "Payment processed successfully."
+        order = @payment.order
+        buyer = order.buyer
+        payment_amount = @payment.amount
+  
+        # Perform the payment processing using your chosen payment gateway or provider.
+        # For example, if you are using a hypothetical 'PaymentGateway' class:
+        payment_gateway = PaymentGateway.new
+        if payment_gateway.process_payment(buyer, payment_amount)
+          # Payment successful
+          @payment.update(payment_status: "Paid", payment_date: Time.now)
+          redirect_to payment_path(@payment), notice: "Payment processed successfully."
+        else
+          # Payment failed
+          @payment.update(payment_status: "Failed", payment_date: Time.now)
+          redirect_to payment_path(@payment), alert: "Payment processing failed. Please try again or contact customer support."
+        end
       else
         render :new
       end
